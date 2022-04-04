c284ca9dc4f3c5f1eb63e96692e82c49405e4631
YOUNGJIN JOO <neoelec@gmail.com>
2022-04-05 00:04:56 +0900

    (dovelet): stair_1/tiles
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ stair_1/tiles/tiles.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int r, c;
+int whole, part;
+
+void __tiles_read(void) {
+	cin >> r >> c;
+}
+
+void __tiles_solve(void) {
+	whole = (r / 8) * (c / 8);
+	part = ((r / 8) + !!(r % 8)) * ((c / 8) + !!(c % 8)) - whole;
+}
+
+void __tiles_print_result(void) {
+	cout << "The number of whole tiles is " << whole << " part tiles is " << part << endl;
+}
+
+int main(int argc, char *argv[]) {
+	tiles();
+
+	return 0;
+}
