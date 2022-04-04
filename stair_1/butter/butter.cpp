a7dfb1fa4d189f2b570247ef022c67ef99f3640e
YOUNGJIN JOO <neoelec@gmail.com>
2022-04-04 23:38:46 +0900

    (dovelet): stair_1/butter
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ stair_1/butter/butter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+void __butter_read(void) {
+	cin >> n >> m;
+}
+
+void __butter_solve_and_print_result(void) {
+	cout << n << endl;
+}
+
+int main(int argc, char *argv[]) {
+	__butter_read();
+	__butter_solve_and_print_result();
+
+	return 0;
+}
